5c45e1306e3961a14255132f4452c675de71ae60
Ali-Sina-255 <alisinasultani255@gmail.com>
2025-05-31 09:44:40 +0430

    category list is done

---
+++ apps/Backend/apps/group/paginations.py
@@ -0,0 +1,5 @@
+from rest_framework.pagination import PageNumberPagination
+
+class OrderPagination(PageNumberPagination):
+    page_size = 20  # Number of items per page
+    page_query_param = "pagenum"  # Custom query parameter name for pagination
